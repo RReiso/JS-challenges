b4d2a270b38ca53fa8ea2900c046df40b326552c
RReiso <73975409+RReiso@users.noreply.github.com>
2021-06-22 23:45:18 -0400

    Finish Palindrome Checker

--- freeCodeCamp/script.js
+++ freeCodeCamp/script.js
@@ -1,8 +1,14 @@
 //Palindrome Checker
 function palindrome(str) {
-	str = str.replace(/[^0-9a-z]/gi, "");
-	console.log(str);
+	str = str.replace(/[^0-9a-z]/gi, "").toLowerCase();
+  let strReverse = str.split("").reverse().join("")
+
+  for (let i = 0; i <str.length; i++){
+    if (str[i]!==strReverse[i]){
+      return false;
+    }
+  }
 	return true;
 }
 
-palindrome("2_A3*3#A2");
+palindrome("1 eye for of 1 eye.")
