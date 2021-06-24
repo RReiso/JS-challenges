dede6bfbb22d41ea936768283d516e8eba2ebf36
RReiso <73975409+RReiso@users.noreply.github.com>
2021-06-23 23:54:16 -0400

    Start Roman Numeral Converter

--- freeCodeCamp/script.js
+++ freeCodeCamp/script.js
@@ -12,3 +12,35 @@ function palindrome(str) {
 }
 
 palindrome("1 eye for of 1 eye.")
+
+
+//Roman Numeral Converter
+function convertToRoman(num) {
+  const symbols = {
+    1: "I",
+    5: "V",
+    10: "X",
+    50: "L",
+    100: "C",
+    500: "D",
+    1000: "M"
+  }
+  let keys = Object.keys(symbols);
+  
+  let roman=""
+
+  const numString = num.toString()
+  for (let i=0; i<numString.length;i++){
+    let remainder = num % (10 *Math.pow(10,i));
+    let number = remainder - remainder % (10 *Math.pow(10,i-1))
+    let power = 10 *Math.pow(10,i-1);
+    console.log(10 *Math.pow(10,i-1));
+    createRoman(number,power );
+
+  }
+  }
+
+
+
+
+convertToRoman(3999);
