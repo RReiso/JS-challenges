b48b7699546f409f166267e870ef5a71f01b69e3
Ruta R <rutareiso@protonmail.com>
2021-10-19 21:08:52 -0400

    Fix indentation

--- codeconnector/limited-levenshteil-leap-2.js
+++ codeconnector/limited-levenshteil-leap-2.js
@@ -1,35 +1,35 @@
 var ladderLength = function (beginWord, endWord, wordList) {
-	const wordSet = new Set(wordList);
-	let queue = [beginWord];
-	res = [beginWord];
+  const wordSet = new Set(wordList);
+  let queue = [beginWord];
+  res = [beginWord];
 
-	while (queue.length) {
-		let found = false;
-		word = queue.shift();
-		console.log(word);
-		console.log(res);
-		if (word === endWord) return res;
-		for (let i = 0; i < word.length; i++) {
-			for (let j = 0; j < 26; j++) {
-				const newWord =
-					word.slice(0, i) + String.fromCharCode(j + 97) + word.slice(i + 1);
+  while (queue.length) {
+    let found = false;
+    word = queue.shift();
+    console.log(word);
+    console.log(res);
+    if (word === endWord) return res;
+    for (let i = 0; i < word.length; i++) {
+      for (let j = 0; j < 26; j++) {
+        const newWord =
+          word.slice(0, i) + String.fromCharCode(j + 97) + word.slice(i + 1);
 
-				if (wordSet.has(newWord)) {
-					wordSet.delete(newWord);
-					console.log(wordSet);
-					res.push(newWord);
-					console.log(res);
-					queue.push(newWord);
-					found = true;
-				}
-			}
-		}
-		if (found === false) {
-			idx = res.indexOf(word);
-			res.splice(idx, 1);
-		}
-	}
-	return null;
+        if (wordSet.has(newWord)) {
+          wordSet.delete(newWord);
+          console.log(wordSet);
+          res.push(newWord);
+          console.log(res);
+          queue.push(newWord);
+          found = true;
+        }
+      }
+    }
+    if (found === false) {
+      idx = res.indexOf(word);
+      res.splice(idx, 1);
+    }
+  }
+  return null;
 };
 
 start = "tot";
--- codeconnector/limited-levenshtein-leap.js
+++ codeconnector/limited-levenshtein-leap.js
@@ -1,32 +1,31 @@
 var ladderLength = function (beginWord, endWord, wordList) {
-	const wordSet = new Set(wordList);
-	let queue = [beginWord];
-	let wordPath = { [beginWord]: [beginWord] };
+  const wordSet = new Set(wordList);
+  let queue = [beginWord];
+  let wordPath = { [beginWord]: [beginWord] };
 
-	while (wordPath) {
-		word = queue.shift();
-    
-		if (word === endWord) return wordPath[word];
-		for (let i = 0; i < word.length; i++) {
-			for (let j = 0; j < 26; j++) {
-				const newWord =
-					word.slice(0, i) + String.fromCharCode(j + 97) + word.slice(i + 1);
+  while (wordPath) {
+    word = queue.shift();
 
-				if (wordSet.has(newWord)) {
-					wordSet.delete(newWord);
-					queue.push(newWord);
-					const path = wordPath[word];
-					let newPath = [...path];
-					newPath.push(newWord);
-					wordPath[newWord] = newPath;
-					console.log(wordPath);
-				}
-			}
-		}
-	}
-	return null;
+    if (word === endWord) return wordPath[word];
+    for (let i = 0; i < word.length; i++) {
+      for (let j = 0; j < 26; j++) {
+        const newWord =
+          word.slice(0, i) + String.fromCharCode(j + 97) + word.slice(i + 1);
+
+        if (wordSet.has(newWord)) {
+          wordSet.delete(newWord);
+          queue.push(newWord);
+          const path = wordPath[word];
+          let newPath = [...path];
+          newPath.push(newWord);
+          wordPath[newWord] = newPath;
+          console.log(wordPath);
+        }
+      }
+    }
+  }
+  return null;
 };
-
 
 start = "tot";
 end = "kid";
