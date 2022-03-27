b8d6b689504f319447ee5b9a1d00495a3a9ae234
Ruta R <73975409+RReiso@users.noreply.github.com>
2022-03-27 17:03:11 -0400

    Add solution with two pointers

---
+++ arrayOfSquares.js
@@ -0,0 +1,26 @@
+// Given an ordered array of integers, return an array of the squared integers in ascending order.
+
+const arrayOfSquares = (arr)=>{
+  const result = new Array(arr.length).fill(0);
+  let left = 0;
+  let right = arr.length - 1
+  let maxUnfilledIdx = arr.length - 1
+  
+  while (left <= right) {
+    let leftSquare = arr[left] * arr[left];
+    let rightSquare = arr[right] * arr[right] 
+
+    if (leftSquare > rightSquare){
+      result[maxUnfilledIdx] = leftSquare
+      left++;
+    } else {
+      result[maxUnfilledIdx] = rightSquare
+      right--;
+    }
+    maxUnfilledIdx--;
+  }
+  return result;
+}
+
+// Time - O(n)
+// Space - O(1)
