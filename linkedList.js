09b8ef334952f274954fb9b2d62971d8c4d27d24
Ruta R <73975409+RReiso@users.noreply.github.com>
2022-04-02 06:14:43 -0400

    Add methods

--- linkedList.js
+++ linkedList.js
@@ -26,6 +26,47 @@ class LinkedList {
     this.head = null;
     this.length = 0;
   }
+  
+  shift() {
+    if (!this.head) return null;
+
+    const oldHead = this.head;
+    this.head = this.head.next;
+    this.length--;
+    return oldHead;
+  }
+
+  pop() {
+    if (!this.length) {
+      return null;
+    }
+
+    if (this.length === 1) {
+      return this.shift();
+    }
+
+    let temp = this.head;
+    let prev = temp;
+
+    while (temp.next) {
+      prev = temp;
+      temp = temp.next;
+    }
+    prev.next = null;
+    this.length--;
+
+    return temp;
+  }
+
+  push(value) {
+    if (!this.head) {
+      return this.unshift(value);
+    }
+    let last = this.getLast();
+    const newNode = new Node(value, null);
+    last.next = newNode;
+    this.length++;
+  }
 }
 
 class Node {
