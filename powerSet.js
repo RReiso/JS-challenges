a0c390b79d37e925a60834b76cf32f306038f9d4
Ruta R <73975409+RReiso@users.noreply.github.com>
2022-07-04 20:01:29 -0400

    Add solution

--- powerSet.js
+++ powerSet.js
@@ -1,1 +1,32 @@
+// Output all subsets from elements of the given array
 
+const powerSet = (nums) => {
+  let allSubsets = [];
+
+  const helper = (i, subset) => {
+    if (i === nums.length) {
+      allSubsets.push([...subset]);
+      return;
+    }
+
+    // don't add element to the subset
+    helper(i + 1, subset);
+    // add element to the subset
+    subset.push(nums[i]);
+    helper(i + 1, subset);
+    subset.pop();
+  };
+  helper(0, []);
+  return allSubsets;
+};
+
+console.log(powerSet([1, 8, 7])); 
+//  [
+//   [],       [ 7 ],
+//   [ 8 ],    [ 8, 7 ],
+//   [ 1 ],    [ 1, 7 ],
+//   [ 1, 8 ], [ 1, 8, 7 ]
+// ]
+
+// Time - O(2^n x n) - pushes to allSubset and average pushes to each subset (n/2 --- simplified = n)
+// Time - O(2^n x n) - subsets and average number of elements in each subset (n/2 --- simplified = n)
