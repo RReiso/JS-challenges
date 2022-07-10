ada88659b71fe939409cb96a2d1d5ad79b974e79
Ruta R <73975409+RReiso@users.noreply.github.com>
2022-07-10 17:17:44 -0400

    Add O(n^2) solution

--- isPalindrome.js
+++ isPalindrome.js
@@ -1,1 +1,17 @@
+// Check if a string is a palindrom
 
+const isPalindrome = (string) => {
+  let reversedString = "";
+
+  for (let i = string.length - 1; i >= 0; i--) {
+    reversedString += string[i];
+  }
+  if (reversedString === string) {
+    return true;
+  }
+  return false;
+
+  // Time - O(n^2) - as in JS strings are immutable, adding to a string means copying the whole
+  // string and adding a new character to the copy.
+  // Space - O(n)
+};
