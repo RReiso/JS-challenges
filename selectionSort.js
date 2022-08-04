f7626a49bba5583accd0d7eb480d03fbb3e3eee4
Ruta R <73975409+RReiso@users.noreply.github.com>
2022-08-03 22:43:45 -0400

    Add another solution

--- selectionSort.js
+++ selectionSort.js
@@ -17,3 +17,26 @@ const selectionSort = (arr) =>{
 
 // Time - O(n^2)
 // Space - O(1)
+
+
+const selectionSort = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    let idxOfSmallest = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[idxOfSmallest]) {
+        idxOfSmallest = j;
+      }
+    }
+    if (idxOfSmallest !== i) {
+      let temp = arr[i];
+      arr[i] = arr[idxOfSmallest];
+      arr[idxOfSmallest] = temp;
+    }
+  }
+  return arr;
+};
+
+console.log(selectionSort([4, 3, 3, 2, 1]));
+
+// Time - O(n^2)
+// Space - O(1)
